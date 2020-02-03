eca39e14e948db96aaa4185a4828f97caec5e5da
cthanavat <tsangrea3@gmail.com>
2020-02-03 17:51:51 +0700

    modified:   programe_testing.py

--- programe_testing.py
+++ programe_testing.py
@@ -3,5 +3,6 @@ import subprocess
 
 if __name__ == "__main__":
     #print ("wow")
+    #wtf
     output = subprocess.run(['python','dnac_phrase_control.py', "device status"],  capture_output=True, text=True, shell=True).stdout
     print (output)
