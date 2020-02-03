af0753a71d7069ee492afd7a85f64a92f8999101
CThna <tsangreal3@gmail.com>
2020-02-04 03:40:45 +0700

    Add files via upload

--- line_connector.py
+++ line_connector.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -37,11 +37,12 @@ def callback():
 def handle_message(event):
     # input from LINE msg
     text = event.message.text
-
-    # THANA custormize
-    info = subprocess.STARTUPINFO()
-    info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    info.wShowWindow = subprocess.SW_HIDE
+    #waht the fuck
+    if os.name == 'Windows':
+        # Configure subprocess to hide the console window
+        info = subprocess.STARTUPINFO()
+        info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = subprocess.SW_HIDE
 
     output = subprocess.run(['python','dnac_phrase_control.py', text],  capture_output=True, text=True, shell=True).stdout
     print (output)
