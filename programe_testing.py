ea6956182babdaa3234af60009bb28a02bdbb06e
CThna <tsangreal3@gmail.com>
2020-02-03 17:54:48 +0700

    Add files via upload

---
+++ programe_testing.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    #print ("wow")
+    #wtf
+    output = subprocess.run(['python','dnac_phrase_control.py', "device status"],  capture_output=True, text=True, shell=True).stdout
+    print (output)
